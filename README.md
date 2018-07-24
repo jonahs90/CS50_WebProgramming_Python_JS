9439ab10fbdc021312925b74a2a29544da259d3b
Jonathan Sanchez <jonathansanchez595@gmail.com>
2018-07-24 01:12:03 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Curso CS50 Web Programming with Python and Javascript
+## 2018
+## Jonathan H. Sánchez
+
+
+# Plataforma: Edx.org
+
+
+El siguiente repositorio es una recopilación de los código fuente ejemplo, comentado y con notas ademas de los codigos de los diferentes proyectos a lo largo del curso.
+
+Puede servir como guía de estudio, yo lo uso mayormente como recordatorio de algunas cosas.
+
+
---
+++ src3/select.sql
@@ -0,0 +1,14 @@
+SELECT * FROM flights;
+SELECT origin, destination FROM flights;
+SELECT * FROM flights WHERE id = 3;
+SELECT * FROM flights WHERE origin = 'New York';
+SELECT * FROM flights WHERE duration > 500;
+SELECT * FROM flights WHERE destination = 'Paris' AND duration > 500;
+SELECT * FROM flights WHERE destination = 'Paris' OR duration > 500;
+SELECT COUNT(*) FROM flights;
+SELECT COUNT(*) FROM flights WHERE origin = 'New York';
+SELECT AVG(duration) FROM flights;
+
+SELECT MIN(duration) FROM flights;
+
+SELECT * FROM flights WHERE origin LIKE '%a%';
